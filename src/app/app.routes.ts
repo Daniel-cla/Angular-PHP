812df41076b8a31193634e88ffc50b0ad2925df0
Daniel <daniel2579c@gmail.com>
2025-05-03 22:28:52 -0500

    Se agrega guard para proteger rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { DetailsComponent } from './components/details/details.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -11,6 +12,7 @@ export const routes: Routes = [
   {
     path: 'details/:id',
     component: DetailsComponent,
+    canActivate: [AuthGuard],
     title: 'Home Details'
   }
 ];
